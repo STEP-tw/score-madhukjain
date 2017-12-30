f06ef5966c0cafb63d2575a0eecb666c2ce39af7
madhurk <madhurk@in-madhurk.local>
2017-12-30 15:31:16 +0530

    Added updateScore function which updates the score.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ let snake=undefined;
 let food=undefined;
 let numberOfRows=60;
 let numberOfCols=120;
+let score = 0;
+
 let animator=undefined;
 
 const animateSnake=function() {
@@ -12,10 +14,15 @@ const animateSnake=function() {
   unpaintSnake(oldTail);
   paintHead(head);
   if(head.isSameCoordAs(food)) {
+    updateScore();
     snake.grow();
     createFood(numberOfRows,numberOfCols);
     drawFood(food);
   }
+}
+
+const updateScore = function(){
+  document.querySelector('#score').innerText = score+=10;
 }
 
 const changeSnakeDirection=function(event) {
